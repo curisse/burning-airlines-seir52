0eeda3910d936fa8c5b1180a5f79fc0365f32074
Aleksander Kalin Nadir Hammo <akn.ham@gmail.com>
2022-04-28 13:32:36 +1000

    working create flights form

--- burning-airplane-client/src/components/CreateFlights.js
+++ burning-airplane-client/src/components/CreateFlights.js
@@ -1,14 +1,53 @@
 import React, {Component} from 'react';
 
 class CreateFlights extends Component {
+    constructor() {
+        super();
+        this.state = { content: ''};
+        this._handleChange = this._handleChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
+    }
+
+    _handleChange(event) {
+        this.setState({content: event.target.value});
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+        console.log('flight created', this.state.content);
+        //TO DO: call the prop passed in from parent
+        this.props.onSubmit(this.state.content);
+        this.setState({content: ''}); //clear form
+    }
+
     render(){
+
         return(
-            <div>
-                <h1>Create Flight</h1>
-            </div>
-        )
+            
+            <form onSubmit={this._handleSubmit}>
+            <h1>Create Flight</h1>
+                        <br></br>
+                <textarea onChange={ this._handleChange} value={this.state.content}></textarea>
+                        <br></br>
+                    <input type="submit" value="Flight Origin"></input>
+                        <br></br><br></br>
+
+                <textarea onChange={ this._handleChange} value={this.state.content}></textarea>
+                        <br></br>
+                    <input type="submit" value="Flight Destination"></input>
+                        <br></br><br></br>
+
+                <textarea onChange={ this._handleChange} value={this.state.content}></textarea>
+                        <br></br>
+                    <input type="submit" value="Flight Date"></input>
+                        <br></br><br></br>
+
+                <textarea onChange={ this._handleChange} value={this.state.content}></textarea>
+                        <br></br>
+                    <input type="submit" value="Flight Number"></input>
+            </form>
+        );
     }
 }
 
-
-export default CreateFlights;
+export default CreateFlights
